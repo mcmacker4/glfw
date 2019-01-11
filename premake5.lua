5d8cbf5ff43cbf5c33486148445f49877f0035c8
Hans <mcmacker4@gmail.com>
2019-01-11 14:16:41 +0100

    Premake5 build file (tested on windows only)

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+buildDir = "%{wks.location}/Build/%{cfg.system}-%{cfg.buildcfg}/%{prj.name}/"
+
+project "GLFW"
+
+    kind "StaticLib"
+    
+    language "C"
+    architecture "x86_64"
+
+    targetdir(buildDir)
+    objdir(buildDir .. ".obj")
+
+    files {
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
+
+    includedirs {
+        "./include/"
+    }
+
+    filter "configurations:Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+    
+    filter "system:windows"
+        defines { "_GLFW_WIN32" }
+        systemversion "latest"
+        files {
+            "src/wgl_context.c",
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_thread.c",
+            "src/win32_time.c",
+            "src/win32_window.c"
+        }
+        links {
+            "gdi32"
+        }
+    
+    filter "system:linux"
+        defines { "_GLFW_X11" }
+        files {
+            "src/glx_context.c",
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/linux_joystick.c"
+        }
+
